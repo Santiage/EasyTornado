cdc4f048ed1e6be3ec35e92230deffa3044a35f9
Santiago-Z <ps5205m@163.com>
2020-09-20 16:20:57 +0800

    2020092002

--- easytornado.py
+++ easytornado.py
@@ -1,18 +1,26 @@
 # -*- coding: utf-8 -*-
 
-from compiler.Compiler import Compiler
-
+# from compiler.Compiler import Compiler
+from editor.databaseTools import dbTools
 
 
 
 if __name__ == '__main__':
-    cc = Compiler()
+    # cc = Compiler()
     # 读取xml文件
-    with open("./test.xml", encoding="utf-8") as fp:
-        content = fp.read()
-        cc.load(content)
-        fp.close()
-        cc.exec()
-
+    # with open("./test.xml", encoding="utf-8") as fp:
+    #     content = fp.read()
+    #     cc.load(content)
+    #     fp.close()
+    #     cc.exec()
+    dbinfo = {
+        "host": "172.16.8.7",
+        "port": "3306",
+        "databasename": "test",
+        "username": "root",
+        "password": "123456"
+    }
+    ret, xmldata = dbTools.db2xml(database = dbinfo)
+    print (xmldata)
         
     
---
+++ editor/databaseTools.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+
+from utils.database import db
+import dicttoxml
+
+
+class dbTools:
+    @staticmethod
+    def db2xml(database:dict):
+        # 判断参数是否齐全
+        if ('host' not in database.keys() or 'databasename' not in database.keys() or 'username' not in database.keys()):
+            return False, None
+       
+
+
+        # 用于生成xml的字典变量
+        dbinfo = {}
+        dbinfo['type'] = "mysql"
+        dbinfo['host'] = database['host']
+        dbinfo['databasename'] = database['databasename']
+        dbinfo['username'] = database['username']
+        if ('port' not in database.keys()):
+            dbinfo['port'] = 3306
+        if ('password' not in database.keys()):
+            dbinfo['password'] = ''
+        dbinfo['schema'] = []
+
+        dbclient = db(database)
+        dbclient.connection()
+
+        # 获得当前数据库下所有表信息
+        tlist = dbclient.getAllTables()
+        print (tlist)
+        for titem in tlist:
+            table = {}
+            table['name'] = titem['table_name']
+            table['comment'] = titem['table_comment']
+            table['fields'] = []
+            # 获得当前表所有字段
+            fields = dbclient.getTableColumns(titem['table_name'])
+            for fitem in fields:
+                field = {}
+                field['name'] = fitem['column_name']
+                field['type'] = fitem['column_type'][0: fitem['column_type'].find("(")]
+                field['length'] = fitem['column_type'][fitem['column_type'].find("(")+1:fitem['column_type'].find(")")]
+                if (fitem['column_key'] == 'PRI'):
+                    field['primary_key'] = 1
+                else:
+                    field['primary_key'] = 0
+                field['description'] = fitem['column_comment']
+                table['fields'].append(field)
+
+            dbinfo['schema'].append(table)
+        dbinfo = {"database": dbinfo}
+        dbxml = dicttoxml.dicttoxml(dbinfo, custom_root="database", root=False).decode("utf-8")
+        return True, dbxml
+
+
+
+
+if __name__ == '__main__':
+    dbinfo = {
+        "host": "172.16.8.7",
+        "port": "3306",
+        "databasename": "test",
+        "username": "root",
+        "password": "123456"
+    }
+    ret, xmldata = dbTools.db2xml(dbinfo)
+    print (xmldata)
